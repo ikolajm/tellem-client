a3ab0600feba5458d332bff34b41cc9ff9db5b7e
JMI <ikolajm@gmail.com>
2021-03-01 12:22:05 -0500

    attempting to fix render issue on outgoing, removed background check in exchange for overall user detail check

--- src/screens/dashboard/friends/Outgoing.js
+++ src/screens/dashboard/friends/Outgoing.js
@@ -13,34 +13,40 @@ export default ({data}) => {
                             data.length > 0 ?
                                 data.map(request => {
                                     let background = {
-                                        background: request.user && request.user.background ? request.user.background : "pink"
+                                        background: request.user.background
                                     }
                                     return (
-                                        <div className="conversation-item">
-                                            {/* User */}
-                                            <div className="user-container">
-                                                <div style={background} className="avatar">
-                                                    <span>{request.user.username.substring(0, 1)}</span>
-                                                </div>
-                                                {/* Name and last message receieved */}
-                                                <div className="information">
-                                                    <h1>{request.user.username}</h1>
-                                                    <h1>#{request.user.idCode}</h1>
-                                                </div>
-                                            </div>
-                                            {/* Last Message */}
-                                            <div className="message">
-                                                {
-                                                    request.user.statusMessage && request.user.statusMessage.trim() !== "" ?
-                                                    "'" + request.user.statusMessage + "'" : <em>No status available</em>
-                                                }
-                                            </div>
-                                            {/* Option menu */}
-                                            <div className="button-nav">
-                                                {/* Create Message */}
-                                                <button>Pending <FaStopwatch /></button>
-                                            </div>
-                                        </div>
+                                        <Fragment>
+                                            {
+                                                request.user.username ?
+                                                    <div className="conversation-item">
+                                                        {/* User */}
+                                                        <div className="user-container">
+                                                            <div style={background} className="avatar">
+                                                                <span>{request.user.username.substring(0, 1)}</span>
+                                                            </div>
+                                                            {/* Name and last message receieved */}
+                                                            <div className="information">
+                                                                <h1>{request.user.username}</h1>
+                                                                <h1>#{request.user.idCode}</h1>
+                                                            </div>
+                                                        </div>
+                                                        {/* Last Message */}
+                                                        <div className="message">
+                                                            {
+                                                                request.user.statusMessage && request.user.statusMessage.trim() !== "" ?
+                                                                "'" + request.user.statusMessage + "'" : <em>No status available</em>
+                                                            }
+                                                        </div>
+                                                        {/* Option menu */}
+                                                        <div className="button-nav">
+                                                            {/* Create Message */}
+                                                            <button>Pending <FaStopwatch /></button>
+                                                        </div>
+                                                    </div>
+                                                : ""
+                                            }
+                                        </Fragment>
                                     )
                                 })
                                 : <h1 className="null-header">No outgoing friend requests</h1>
